af0cb5aec7bb4dc24d3eb31ef16bd261eccec6a2
AndreToni <tonifoguel@gmail.com>
2024-08-25 00:09:26 -0300

    commit-52

--- src/app/(main)/pontos-turisticos/page.tsx
+++ src/app/(main)/pontos-turisticos/page.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useEffect } from "react";
 import { CardDefault } from "@/components/cards/CardDefault";
 import { InputSearch } from "@/components/form-components/InputSearch";
 import { fetchData } from "@/services/fetch";
@@ -5,18 +8,26 @@ import VLibras from 'vlibras-nextjs';
 
 export default async function Eventos({searchParams}) {
     const { results } = await fetchData(`tourist-attractions?${searchParams?.input && `input=${searchParams?.input}`}`, 0);
-    
+
     return (
         <div className="flex flex-col gap-6 py-6 w-full max-w-7xl max-lg:px-[25px] mx-auto">
             <h1 className="font-heading1">Pontos turísticos</h1>
             <InputSearch />
             <div className="grid lg:grid-cols-3 xl:grid-cols-4 gap-6">
-                {
-                    results.map(item => <CardDefault key={item.id} place={item} />)
-                }
+                {results.map(item => <CardDefault key={item.id} place={item} />)}
             </div>
-            {process.env.NODE_ENV === "production" && <VLibras forceOnload />}
+            <ClientOnlyVLibras />
         </div>
-        
     )
+}
+
+function ClientOnlyVLibras() {
+    useEffect(() => {
+        if (process.env.NODE_ENV === "production") {
+            // Inicializa o VLibras somente no cliente
+            new VLibras({ forceOnload: true });
+        }
+    }, []);
+
+    return null;
 }
