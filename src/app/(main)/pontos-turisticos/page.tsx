35c418c3566b838387fb38d361659abcbfdaafaf
AndreToni <tonifoguel@gmail.com>
2024-08-24 23:49:12 -0300

    commit-46

--- src/app/(main)/pontos-turisticos/page.tsx
+++ src/app/(main)/pontos-turisticos/page.tsx
@@ -1,31 +1,22 @@
-import { useEffect, useState } from 'react';
 import { CardDefault } from "@/components/cards/CardDefault";
 import { InputSearch } from "@/components/form-components/InputSearch";
 import { fetchData } from "@/services/fetch";
 import VLibras from 'vlibras-nextjs';
 
-export default function Eventos({ searchParams }) {
-    const [results, setResults] = useState([]);
-
-    useEffect(() => {
-        const fetchDataAsync = async () => {
-            const { results } = await fetchData(`tourist-attractions?${searchParams?.input && `input=${searchParams?.input}`}`, 0);
-            setResults(results);
-        };
-
-        fetchDataAsync();
-    }, [searchParams]);
-
+export default async function Eventos({searchParams}) {
+    const { results } = await fetchData(`tourist-attractions?${searchParams?.input && `input=${searchParams?.input}`}`, 0);
+    
     return (
         <div className="flex flex-col gap-6 py-6 w-full max-w-7xl max-lg:px-[25px] mx-auto">
             <h1 className="font-heading1">Pontos turísticos</h1>
             <InputSearch />
             <div className="grid lg:grid-cols-3 xl:grid-cols-4 gap-6">
-                {results.map(item => (
-                    <CardDefault key={item.id} place={item} />
-                ))}
+                {
+                    results.map(item => <CardDefault key={item.id} place={item} />)
+                }
             </div>
             {process.env.NODE_ENV === "production" && <VLibras forceOnload />}
         </div>
-    );
+        
+    )
 }
