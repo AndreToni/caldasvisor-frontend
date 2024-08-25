f8c6ab8951ab96b679c64f9820ad6388ca3645d5
AndreToni <tonifoguel@gmail.com>
2024-08-24 22:20:03 -0300

    commit-32

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { AuthProvider } from '@/contexts/auth'
 import { cookies } from 'next/headers'
+import VLibras from 'vlibras-nextjs';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -25,6 +26,7 @@ export default function RootLayout({
         <AuthProvider user={user}>
           {children}
         </AuthProvider>
+        {process.env.NODE_ENV === "production" && <VLibras forceOnload />}
       </body>
     </html>
   )
