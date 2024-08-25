6d431a49ef0b03c9ffaa8158bdb353fa09cdee0b
AndreToni <tonifoguel@gmail.com>
2024-08-25 14:06:02 -0300

    commit-56

---
+++ src/app/(main)/eventos/content.tsx
@@ -0,0 +1,19 @@
+import { CardDefault } from "@/components/cards/CardDefault";
+import { InputSearch } from "@/components/form-components/InputSearch";
+import VLibras from 'vlibras-nextjs';
+
+
+export default async function EventosContent({results}) {
+    return (
+        <div className="flex flex-col gap-6 py-6 w-full max-w-7xl max-lg:px-[25px] mx-auto">
+            <h1 className="font-heading1">Eventos</h1>
+            <InputSearch />
+            <div className="grid lg:grid-cols-3 xl:grid-cols-4 gap-6">
+                {
+                    results.map(item => <CardDefault key={item.id} place={{...item, type: 'event'}} />)
+                }
+            </div>
+            {process.env.NODE_ENV === "production" && <VLibras forceOnload />}
+        </div>
+    )
+}
--- src/app/(main)/eventos/page.tsx
+++ src/app/(main)/eventos/page.tsx
@@ -1,21 +1,11 @@
-import { CardDefault } from "@/components/cards/CardDefault";
-import { InputSearch } from "@/components/form-components/InputSearch";
 import { fetchData } from "@/services/fetch";
+import EventosContent from "./content";
 
 
 export default async function Eventos({searchParams}) {
     const { results } = await fetchData(`events?${searchParams?.input && `input=${searchParams?.input}`}`, 0);
 
     return (
-        <div className="flex flex-col gap-6 py-6 w-full max-w-7xl max-lg:px-[25px] mx-auto">
-            <h1 className="font-heading1">Eventos</h1>
-            <InputSearch />
-            <div className="grid lg:grid-cols-3 xl:grid-cols-4 gap-6">
-                {
-                    results.map(item => <CardDefault key={item.id} place={{...item, type: 'event'}} />)
-                }
-            </div>
-
-        </div>
+        <EventosContent results={results}/>
     )
 }
