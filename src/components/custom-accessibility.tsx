3968c9eed2312e1ca936547ae565876db2723b5e
AndreToni <tonifoguel@gmail.com>
2024-09-11 01:32:19 -0300

    commit-58

--- src/components/custom-accessibility.tsx
+++ src/components/custom-accessibility.tsx
@@ -1,8 +1,15 @@
 'use client'
 
+import { useEffect, useState } from 'react';
 import VLibras from 'vlibras-nextjs';
 
 export function CustomAccessibility() {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+    
     return (
         <>
             {process.env.NODE_ENV === "production" && <VLibras forceOnload />}
